132e1e2d6c351dc2a97db54c4466b0ac7f880d13
Dave Turner <davetii@gmail.com>
2024-03-26 19:51:00 -0400

    Adding dery run to cucumber test

--- src/test/java/software/daveturner/gametime/CucumberRunner.java
+++ src/test/java/software/daveturner/gametime/CucumberRunner.java
@@ -7,6 +7,7 @@ import org.junit.runner.*;
 @CucumberOptions(
         features = {"src/test/resources/features"},
         plugin = {"pretty"},
+        dryRun = true,
         glue = {"software.daveturner.gametime.cucumber"})
 public class CucumberRunner {
 }
